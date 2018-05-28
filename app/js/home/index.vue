d20c866874658168b6464c051ffb97a7e39a06b1
unknown <753100785@qq.com>
2018-05-28 20:44:32 +0800

    first

---
+++ app/js/home/index.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+    <div class="home">
+        <h1>
+            home
+            <p class="title">title</p>    
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {   
+}
+</script>
+
+<style lang="css">
+@import "../../css/reset.scss";
+  .home{
+    color: red;
+    font-size: 80px;
+}
+</style>
